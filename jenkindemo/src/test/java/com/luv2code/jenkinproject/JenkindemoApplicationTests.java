4b6dfb510ce2d3b79ae13305a40496c1fa5704ff
Pradeep Yadav <pradpyadav@gmail.com>
2020-05-28 16:15:21 -0400

    First commit

---
+++ jenkindemo/src/test/java/com/luv2code/jenkinproject/JenkindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.jenkinproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
